5101ff8aaab5fbb139bed9c02aca54e81e0fb358
itsuki-jp <itsuki19601@gmail.com>
2022-06-26 00:50:40 +0900

    プレイヤーの移動

--- 2d/maze/index.js
+++ 2d/maze/index.js
@@ -19,6 +19,7 @@ class Maze {
         }
     }
 
+    // 盤面を初期化
     initMaze() {
         for (let i = 0; i < this.h + 2; i++) {
             this.mazeMap.push(new Array());
@@ -33,12 +34,15 @@ class Maze {
             }
         }
     }
+
+    // 1_1->[1,1] みたいに、アンダーバーで分ける 
     splitPos2Two(posStr) {
         let splitted = posStr.split("_");
         return [Number(splitted[0]), Number(splitted[1])];
     }
+
+    // 迷路を生成(壁伸ばし方)
     createMaze() {
-        console.log("---------------------");
         let [y, x] = this.splitPos2Two(this.availableRoad[Math.floor(Math.random() * this.availableRoad.length)]);
         let newWall = new Set([`${y}_${x}`]);
         while (true) {
@@ -71,6 +75,8 @@ class Maze {
         }
         return true;
     }
+
+    // 作成された迷路をdocument上に書き込む
     drawOnDocument() {
         for (let i = 0; i < 13; i++) {
             for (let j = 0; j < 17; j++) {
@@ -79,6 +85,8 @@ class Maze {
             document.write("<br>");
         }
     }
+
+    // 作成された迷路をcanvas上に書き込む
     drawOnCanvas(ctx) {
         ctx.beginPath();
         ctx.fillStyle = "black";
@@ -94,7 +102,55 @@ class Maze {
         ctx.closePath();
     }
 }
+class Player {
+    constructor(x, y, mazeMap) {
+        this.x = x;
+        this.y = y;
+        this.mazeMap = mazeMap;
+        this.drawPlayer();
+    }
 
-let newMaze = new Maze(Y, X);
+    drawPlayer() {
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.fillRect(W / (X + 2) * this.x, H / (Y + 2) * this.y, W / (X + 2), H / (Y + 2));
+        ctx.closePath();
+    }
+    convertEventKey2dydx(event) {
+        let directionDict = {
+            "ArrowRight": [0, 1],
+            "ArrowLeft": [0, -1],
+            "ArrowDown": [1, 0],
+            "ArrowUp": [-1, 0]
+        };
+        if (event.key in directionDict) { return directionDict[event.key]; }
+        return false;
+    }
+    isAvailable(event) {
+        let dydx = this.convertEventKey2dydx(event);
+        if (!dydx) { return false; }
+        let [dy, dx] = dydx;
+        if (this.mazeMap[this.y + dy][this.x + dx] === "#") { return false; }
+        return true;
+    }
+
+    move(event) {
+        let dydx = this.convertEventKey2dydx(event);
+        if (!dydx) { return false; }
+        let [dy, dx] = dydx;
+        this.x += dx;
+        this.y += dy;
+    }
+}
+
+const newMaze = new Maze(Y, X);
 newMaze.drawOnCanvas(ctx);
-newMaze.drawOnDocument();
+const player = new Player(1, 1, newMaze.mazeMap);
+
+document.onkeydown = event => {
+    if (player.isAvailable(event)) {
+        newMaze.drawOnCanvas(ctx);
+        player.move(event);
+        player.drawPlayer();
+    }
+}
