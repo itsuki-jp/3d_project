d44ba88229927a637708484395c6487a3a09bd8a
itsuki-jp <itsuki19601@gmail.com>
2022-06-26 01:00:54 +0900

    branch test

--- 2d/maze/index.js
+++ 2d/maze/index.js
@@ -4,7 +4,7 @@ let [Y, X] = [21, 21];
 let [H, W] = [Y * 30, X * 30];
 canvas.width = W;
 canvas.height = H;
-
+console.log("branch test");
 const newMaze = new Maze(Y, X);
 newMaze.drawOnCanvas(ctx);
 const player = new Player(1, 1, newMaze.mazeMap);
